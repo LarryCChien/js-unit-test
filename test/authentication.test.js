b81462b26d5929e32a0e0925aed140ec288dcd8f
Larry Chien <larry.c.chien@gmail.com>
2021-04-10 15:47:26 +0800

    重構並加上 invalid

--- test/authentication.test.js
+++ test/authentication.test.js
@@ -1,19 +1,45 @@
-import { jest } from '@jest/globals'
+import { beforeEach, jest } from '@jest/globals'
 import { Authentication } from '../src/authentication'
 import * as otp from '../src/otp'
 
 jest.mock('../src/otp')
 
 describe('authenticate account is valid', () => {
-  it('should be valid', () => {
-    let authentication = new Authentication()
-
-    const fakeGetPassword = jest.fn()
+  let authentication = new Authentication()
+  let fakeGetPassword
+  // let fakeGetToken
+  let givenPassword
+  let givenToken
+  beforeEach(() => {
+    fakeGetPassword = jest.fn()
     authentication.getPassword = fakeGetPassword
-    fakeGetPassword.mockReturnValueOnce('91')
 
-    otp.getToken.mockReturnValueOnce('000000')
+    givenPassword = (password) => {
+      fakeGetPassword.mockReturnValueOnce(password)
+    }
 
-    expect(authentication.isValid('joey', '91000000')).toBe(true)
+    givenToken = (token) => {
+      otp.getToken.mockReturnValueOnce(token)
+    }
+  })
+
+  const shouldBeValid = (password) => {
+    expect(authentication.isValid('joey', password)).toBe(true)
+  }
+
+  it('should be valid', () => {
+    givenPassword('91')
+    givenToken('000000')
+    shouldBeValid('91000000')
+  })
+
+  const shouldBeInvalid = (password) => {
+    expect(authentication.isValid('joey', password)).toBe(false)
+  }
+
+  it('should be invalid', () => {
+    givenPassword('91')
+    givenToken('000000')
+    shouldBeInvalid('wrong password')
   })
 })
