68cc574e9cdf536e56b503154416e095882b2e2e
Larry Chien <larry.c.chien@gmail.com>
2021-04-10 18:18:27 +0800

    Order service 加上測試

--- src/order_service.js
+++ src/order_service.js
@@ -1,50 +1,56 @@
-import {BookDao} from "./book_dao";
+/* eslint-disable class-methods-use-this */
+import { BookDao } from './book_dao'
 
 export class OrderService {
-    sync_book_orders() {
-        let orders = this.get_orders();
-        console.log(orders);
+  syncBookOrders() {
+    let orders = this.getOrders()
+    // console.log(orders)
 
-        orders.filter((order) => {
-            return order.orderType === "Book";
-        }).forEach((order) => {
-            let bookDao = new BookDao();
-            bookDao.insert(order);
-        });
+    orders.filter((order) => {
+      return order.orderType === 'Book'
+    }).forEach((order) => {
+      this.bookInsert(order)
+    })
+  }
+
+  bookInsert(order) {
+    let bookDao = new BookDao()
+    bookDao.insert(order)
+  }
+
+  // 基本上這個 method 是跑不動的，因為此次重點是練習測試
+  getOrders() {
+    const parse = require('csv-parse')
+    const fs = require('fs')
+    const fsPromises = fs.promises
+    const path = require('path')
+
+    const inputFilePath = path.resolve(__dirname, './orders.csv')
+
+    return main()
+
+    async function main() {
+      const inputFile = await fsPromises.readFile(inputFilePath)
+      const parsedResult = await parseCSV(inputFile, {
+        delimiter: ',',
+        columns: true,
+      })
+
+      console.log('parsedResult', parsedResult)
+      return parsedResult
     }
 
-    get_orders() {
-        const parse = require('csv-parse');
-        const fs = require('fs');
-        const fsPromises = fs.promises;
-        const path = require('path');
+    function parseCSV(input, options) {
+      return new Promise((resolve, reject) => {
+        parse(input, options, (error, output) => {
+          if (error) {
+            console.error('[ERROR] parseCSV: ', error.message)
+            reject('[ERROR] parseCSV: ', error.message)
+          }
 
-        const inputFilePath = path.resolve(__dirname, './orders.csv');
-
-        return main();
-
-        async function main() {
-            const inputFile = await fsPromises.readFile(inputFilePath);
-            const parsedResult = await parseCSV(inputFile, {
-                delimiter: ',',
-                columns: true,
-            });
-
-            console.log('parsedResult', parsedResult);
-            return parsedResult;
-        }
-
-        function parseCSV(input, options) {
-            return new Promise((resolve, reject) => {
-                parse(input, options, (error, output) => {
-                    if (error) {
-                        console.error('[ERROR] parseCSV: ', error.message);
-                        reject('[ERROR] parseCSV: ', error.message);
-                    }
-
-                    resolve(output);
-                });
-            });
-        }
+          resolve(output)
+        })
+      })
     }
+  }
 }
--- test/order_service.test.js
+++ test/order_service.test.js
@@ -1,8 +1,34 @@
-import {OrderService} from "../src/order_service";
+import { expect } from '@jest/globals'
+import { OrderService } from '../src/order_service'
 
-describe('sync book orders', function () {
-    it('should only sync book orders', () => {
-        // let orderService = new OrderService();
-        // orderService.sync_book_orders();
-    });
-});
+describe('sync book orders', () => {
+  let orderService
+  let fakeOrders
+  let fakeInsert
+  beforeEach(() => {
+    fakeOrders = jest.fn()
+    fakeInsert = jest.fn()
+    orderService = new OrderService()
+
+    orderService.bookInsert = fakeInsert
+  })
+
+  const givenOrders = () => {
+    orderService.getOrders = fakeOrders.mockReturnValueOnce([
+      { orderType: 'Book', price: 100 },
+      { orderType: 'Fruit', price: 250 },
+      { orderType: 'Book', price: 150 },
+    ])
+  }
+  const shouldInsertBookOrder = (number) => {
+    orderService.syncBookOrders()
+    expect(fakeInsert).toBeCalledTimes(number)
+    expect(fakeInsert.mock.calls[0][0]).toEqual({ orderType: 'Book', price: 100 })
+    expect(fakeInsert.mock.calls[1][0]).toEqual({ orderType: 'Book', price: 150 })
+  }
+
+  it('should only sync book orders', () => {
+    givenOrders()
+    shouldInsertBookOrder(2)
+  })
+})
